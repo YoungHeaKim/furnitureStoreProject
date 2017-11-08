d4e68620f732f993db2db009ed6d986220059854
JeongwonKim <swtpumpkin90@gmail.com>
2017-11-08 14:35:50 +0900

    메인 리스트 데이터 출력 완료(아직 데이터 주고받을 내용에대한 토의가 필요함)

--- backend/src/query/listquery.js
+++ backend/src/query/listquery.js
@@ -4,6 +4,8 @@ module.exports = {
   joinBoardData() {
     return knex('board')
       .join('board_type', 'board_type.id', 'board.board_type')
+      .join(`user`, 'user.id', 'board.creator')
+      .select('board.id', 'board.title', 'board.content', knex.raw("DATE_FORMAT(board.created_at, '%Y-%m-%d') as created_at"), 'board.notify_type', 'board.creator', 'board.board_type', 'board_type.id', 'board_type.type', 'user.nickname')
   },
   getBoardTypeById({board_type_id}) {
     const board_type = board_type_id
@@ -11,5 +13,8 @@ module.exports = {
     query.where(board_type)
       .orderBy('board.id', 'desc')
     return query
+  },
+  getBasicList() {
+    return this.joinBoardData().orderBy('board.id', 'desc')
   }
 }
--- backend/src/router/list.js
+++ backend/src/router/list.js
@@ -1,5 +1,9 @@
 const mw = require('../middleware')
 const query = require('../query')
+const express = require('express')
+const app = express()
+
+const router = express.Router()
 
 router.use(mw.bodyParserJsonMiddleware)
 router.use(mw.bodyParserUrlEncodedMiddleware)
@@ -7,5 +11,8 @@ router.use(mw.corsMiddleware)
 
 // 루트에 뿌려줄 게시판 리스트
 router.get('/', (req, res) => {
-  
-}
+  query.getBasicList()
+    .then(list => res.send(list))
+})
+
+module.exports = router
