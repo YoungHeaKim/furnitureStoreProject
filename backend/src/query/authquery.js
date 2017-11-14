b21a622e409dfd30a184bf8bbf212cb6bed0363b
JeongwonKim <swtpumpkin90@gmail.com>
2017-11-14 14:33:42 +0900

    아이디 생성 쿼리에서 중복되는 쿼리 삭제 및 쿼리명 변경

--- backend/src/query/authquery.js
+++ backend/src/query/authquery.js
@@ -1,35 +1,23 @@
 const knex = require('../knex')
 
 module.exports = {
-  getLocalUserById({user_id, nickname}) {
+  getUserByUserIdAndNickname({user_id, nickname}) {
     return knex('user')
       .where({'provider': 'local', user_id, nickname})
       .first()
   },
-  checkAlreadyJoinId({user_id}) {
+  getUserByUserId({user_id}) {
     return knex('user')
     .where({'provider': 'local', user_id})
     .first()
   },
   createUser({user_id, password, nickname}) {
     return knex('user')
-    .where({
-      'provider': 'local',
-      user_id
-    })
-    .first()
-    .then(user => {
-      if (user) {
-        return user
-      } else {
-        return knex('user')
-          .insert({
-            'provider': 'local',
-            user_id,
-            'access_token': password,
-            'nickname': nickname
-         })
-      }
+      .insert({
+        'provider': 'local',
+        user_id,
+        'access_token': password,
+        'nickname': nickname
     })
   },
 }
